c5337787e8f2665a1f06e8894489ab9b531f2ec8
Evelyn(이현수) <mpcovstraw@gmail.com>
2024-09-19 14:05:13 +0900

    fix: site.config.js avatar.png

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "Evelyn Lee",
-    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: "/avatar.png", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "Data Scientist",
     bio: "pathfinder",
     email: "mpcovstraw@gmail.com",
